8008f3a2a42e5eee2e3c5e31ae9a71bb5141f81a
Jamie Phelps <james.phelps1995@live.com>
2020-09-28 10:57:44 +0100

    added sticky navbar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.menu-icon');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', (e) => {
+  nav.classList.add('addNav')
+})
