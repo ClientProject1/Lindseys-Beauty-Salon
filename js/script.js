5437d2af79c789bd9f1c1aa37a312c4ac2fd9a0e
Jamie Phelps <james.phelps1995@live.com>
2020-09-29 10:47:33 +0100

    added working hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.menu-icon');
-const nav = document.querySelector('nav');
+const nav = document.querySelector('.hamburger-navs');
+const menuIcon = document.querySelector('.menu-icon');
 
 hamburger.addEventListener('click', (e) => {
-  nav.classList.add('addNav')
+  if (nav.classList.contains("addNav")) {
+    nav.classList.remove('addNav')
+    menuIcon.src = "img/menu-icon.svg";
+  } else {
+    nav.classList.add('addNav');
+    menuIcon.src = "img/close.svg";
+  }
 })
