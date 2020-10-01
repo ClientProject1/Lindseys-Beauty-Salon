7d77bf4b21c21be66c3e963951777f4b701f6861
Jamie Phelps <james.phelps1995@live.com>
2020-10-01 19:39:53 +0100

    added location.html

--- js/script.js
+++ js/script.js
@@ -17,3 +17,26 @@ hamburger.addEventListener('click', (e) => {
 window.addEventListener("hashchange", function () {
     window.scrollTo(window.scrollX, window.scrollY - 86);
 });
+
+//map
+mapboxgl.accessToken = 'pk.eyJ1IjoiMnBoZWxqNTYiLCJhIjoiY2tmcXBvemdkMGdkazJ3b2Zza3dkbXFwaiJ9.VTG7wmphDY9NM4u2nxC3PQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v10',
+  center: [-1.605367,52.138660], // starting position
+  zoom: 17
+});
+
+// add icon
+var marker = new mapboxgl.Marker()
+.setLngLat([-1.605367, 52.138660])
+.addTo(map);
+
+// initialize the map canvas to interact with later
+var canvas = map.getCanvasContainer();
+
+// an arbitrary start will always be the same
+// only the end or destination will change
+var start = [-122.662323, 45.523751];
+
+// this is where the code for the next step will go
